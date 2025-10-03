5d9ce5f7744795bc1fa466e066fd2040631a4f26
Arunraj-Jaganathan <arunrajjaganathan05@gmail.com>
2025-10-03 10:08:37 +0530

    feat- Initial commit

---
+++ CourseRegistrationSystemBackEnd/src/main/java/com/example/CourseRegistrationSystem/controller/CourseController.java
@@ -0,0 +1,35 @@
+package com.example.CourseRegistrationSystem.controller;
+
+import com.example.CourseRegistrationSystem.model.Course;
+import com.example.CourseRegistrationSystem.model.CourseRegistry;
+import com.example.CourseRegistrationSystem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:5500", "http://127.0.0.1:5500/"})
+public class CourseController {
+
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("/courses")
+    public List<Course> availableCourses() {
+        return courseService.availableCourses();
+    }
+
+    @GetMapping("/courses/enrolled")
+    public List<CourseRegistry> enrolledStudents() {
+        return courseService.enrolledStudent();
+    }
+
+    @PostMapping("courses/register")
+    public String enrolledCourse(@RequestParam("name") String name,
+                                 @RequestParam("emailId") String emailId,
+                                 @RequestParam("courseName") String courseName) {
+        courseService.enrolledCourse(name, emailId, courseName);
+        return "Congratulations! " + name + ". Your enrollment for " + courseName + " is Successful";
+    }
+}
---
+++ CourseRegistrationSystemFrontEnd/index.js
@@ -0,0 +1,37 @@
+function showCourses(){
+    fetch("http://localhost:8080/courses")
+    .then((response) => response.json())
+    .then((courses) => {
+        let dataTable = document.getElementById("coursetable");
+
+        courses.forEach(course => {
+            var row = `<tr>
+            <td>${course.courseId}</td>
+            <td>${course.courseName}</td>
+            <td>${course.trainer}</td>
+            <td>${course.durationInWeeks}</td>
+            </tr>`
+
+            dataTable.innerHTML += row;
+        })
+    })
+}
+
+function enrolledStudents(){
+    fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        let enrolledTable = document.getElementById("enrolledtable");
+        let rows = "";
+        students.forEach(student => {
+            rows += `<tr>
+            <td>${student.id}</td>
+            <td>${student.name}</td>
+            <td>${student.emailId}</td>
+            <td>${student.courseName}</td>
+            </tr>`;
+
+            enrolledTable.innerHTML = rows;
+        });
+    });
+}
